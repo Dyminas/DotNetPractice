5c54b9989b25a4b15737c053eee1eda8cccb0930
纯天然咸鱼 <Dyminas@outlook.com>
2022-01-21 22:37:27 +0800

    Get windows user in .NET application

---
+++ WpfApp/Domain/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.Security.Principal;
+
+namespace WpfApp.Domain
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public string WindowsIdentityUserName { get; }
+        public string UserPrincipalDisplayName { get; }
+        public string UserFolder { get; }
+
+        public HomeViewModel()
+        {
+            var windowsIdentity = WindowsIdentity.GetCurrent();
+            WindowsIdentityUserName = windowsIdentity.Name;
+            var principal = UserPrincipal.Current;
+            UserPrincipalDisplayName = principal.DisplayName;
+            UserFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+    }
+}
